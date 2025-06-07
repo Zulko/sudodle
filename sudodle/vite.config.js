e39f656f48fcc3e2a1f1e1496fbea5069b326f51
Zulko <valentin.zulkower+git@gmail.com>
2025-06-07 12:50:16 -0400

    vite config for github pages

--- sudodle/vite.config.js
+++ sudodle/vite.config.js
@@ -4,4 +4,5 @@ import { svelte } from '@sveltejs/vite-plugin-svelte'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [svelte({runes: true})],
-})
+  base: process.env.NODE_ENV === "production" ? "/sudodle/" : "/",
+});
